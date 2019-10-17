41bc12d3c21493f3eefdc00668860dc123666dc2
matamong <matamatamong@gmail.com>
2019-10-17 17:38:09 +0900

    Update guestbook Project...exception exist

--- BackUp/guestbook/src/main/java/kr/or/connect/guestbook/dto/Guestbook.java
+++ BackUp/guestbook/src/main/java/kr/or/connect/guestbook/dto/Guestbook.java
@@ -1,6 +1,6 @@
 package kr.or.connect.guestbook.dto;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Guestbook {
 	private String content;
---
+++ BackUp/guestbook/src/main/java/kr/or/connect/guestbook/service/Impl/GuestbookServiceImpl.java
@@ -0,0 +1,66 @@
+package kr.or.connect.guestbook.service.Impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.connect.guestbook.dao.GuestbookDao;
+import kr.or.connect.guestbook.dao.LogDao;
+import kr.or.connect.guestbook.dto.Guestbook;
+import kr.or.connect.guestbook.dto.Log;
+import kr.or.connect.guestbook.service.GuestbookService;
+
+@Service
+public class GuestbookServiceImpl implements GuestbookService{
+	@Autowired
+	GuestbookDao guestbookDao;
+	
+	@Autowired
+	LogDao logDao;
+	
+	@Override
+	@Transactional 	  //(내부적으로 Read Only 메서드로 사용하게 됨)
+	public List<Guestbook> getGuestbooks(Integer start) {
+		List<Guestbook> list = guestbookDao.selectAll(start, GuestbookService.LIMIT);
+		return list;
+	}
+
+	@Override
+	@Transactional(readOnly=false) //ReadOnly 되면 안되니깐 false로
+	public int deleteGuestbook(Long id, String ip) {
+		int deleteCount = guestbookDao.deleteById(id);
+		
+		Log log = new Log();
+		log.setIp(ip);
+		log.setMethod("delete");
+		log.setRegdate(new Date());
+		logDao.insert(log);
+		
+		return deleteCount;
+	}
+
+	@Override
+	@Transactional(readOnly=false) //ReadOnly 되면 안되니깐 false로
+	public Guestbook addGuestbook(Guestbook guestbook, String ip) {
+		guestbook.setRegdate(new Date());
+		Long id = guestbookDao.insert(guestbook);
+		guestbook.setId(id);
+		
+		Log log = new Log();
+		log.setIp(ip);
+		log.setMethod("insert");
+		log.setRegdate(new Date());
+		logDao.insert(log);
+		
+		return guestbook;
+	}
+
+	@Override
+	public int getCount() {
+		return guestbookDao.selectCount();
+	}
+	
+}
