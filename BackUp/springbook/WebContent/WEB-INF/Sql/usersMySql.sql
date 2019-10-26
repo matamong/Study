a89f52e07519bb1221fce607d6cf56d62d2e765c
matamong <matamatamong@gmail.com>
2019-10-26 15:47:14 +0900

    Create usersMySql.sql For MySql

---
+++ BackUp/springbook/WebContent/WEB-INF/Sql/usersMySql.sql
@@ -0,0 +1,22 @@
+create user me@localhost identified by ‘tiger’ 
+
+create database me; 
+
+grant all privileges on me.* to me@localhost; 
+
+flush privileges; 
+
+ 
+
+create database me;
+
+use me;
+
+create table users(
+	Id varchar(10) primary key,
+	Name varchar(20) not null,
+	Password varchar(20) not null,
+	Level tinyint not null,
+	Login int not null,
+	Recommend int not null
+);
