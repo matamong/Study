1b201f3f7a96a0a54ea00899f8f8bc220b226b12
matamong <matamatamong@gmail.com>
2019-10-28 19:52:44 +0900

    Modify Some Details

--- BackUp/springbook/src/springbook/user/service/UserServiceTx.java
+++ BackUp/springbook/src/springbook/user/service/UserServiceTx.java
@@ -8,7 +8,7 @@ import springbook.user.domain.User;
 
 public class UserServiceTx implements UserService{
 
-	UserService userService;
+	UserService userService;   //타깃 오브젝트
 	PlatformTransactionManager transactionManager;
 	
 	//UserService를 구현한 다른 오브젝트를 주입받는다.
@@ -20,18 +20,21 @@ public class UserServiceTx implements UserService{
 		this.transactionManager = transactionManager;
 	}
 	
-	@Override   //DI 받은 UserService 오브젝트에 모든 기능을 위임한다.
+	@Override   //DI 받은 UserService 오브젝트에 모든 기능을 위임한다.(메소드 구현과 위임)
 	public void add(User user) {
 		userService.add(user);
 	}
 	
-	@Override   ////DI 받은 UserService 오브젝트에 모든 기능을 위임한다.
+	@Override   //메소드 구현
 	public void upgradeLevels() {
+		//부가기능 수행
 		TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
 		
 		try {
+			//위임
 			userService.upgradeLevels();
 			
+			//부가기능 수행
 			this.transactionManager.commit(status);
 		} catch (RuntimeException e) {
 			this.transactionManager.rollback(status);
