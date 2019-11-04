0b517f261005293ff46b531ae5ec63af0030a750
matamong <matamatamong@gmail.com>
2019-11-04 22:47:20 +0900

    Error pop up...

--- BackUp/apiTest/src/main/java/apiTest/kakaoMap/RequestToKamap.java
+++ BackUp/apiTest/src/main/java/apiTest/kakaoMap/RequestToKamap.java
@@ -10,14 +10,18 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
 public class RequestToKamap {
-	public void getConnect() {
-		String APIKey = "";
+	public static void main(String[] args) {
+		getConnect();
+	}
+	public static void getConnect() {
+		String APIKey = "6740ac764429e04197110a045a9b8482";
 		
 		HashMap<String, Object> map = new HashMap(); //결과 담을 map
 		
 		//Unirest 라이브러리를 이용해서 Http 요청을 보내서 접속
 		try {
-			String apiURL = "" + URLEncoder.encode("address", "UTF-8");
+			String apiURL = "https://dapi.kakao.com/v2/local/search/address.json?query=" 
+							+ URLEncoder.encode("address", "UTF-8");
 			HttpResponse<JsonNode> response = Unirest.get(apiURL)				//해당 API URL에서
 												.header("Authorization", APIKey) // APIKey를 이용해서 권한을 얻는다.
 												.asJson();						// response는 JSon으로
@@ -31,7 +35,8 @@ public class RequestToKamap {
 			KakaoGeoRes bodyJson = objectMapper.readValue(response.getBody().toString(), KakaoGeoRes.class);
 			
 			
-			bodyJson.getDocuments().get(0).getX();
+			double x = bodyJson.getDocuments().get(0).getX();
+			System.out.println(x);
 			bodyJson.getDocuments().get(0).getY();
 			
 
