f3c5d886a6f9f35f061e7f830df761b412bae561
matamong <matamatamong@gmail.com>
2019-11-04 22:00:46 +0900

    Server still working good

--- BackUp/apiTest/src/main/java/apiTest/kakaoMap/RequestToKamap.java
+++ BackUp/apiTest/src/main/java/apiTest/kakaoMap/RequestToKamap.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 
 public class RequestToKamap {
@@ -13,11 +14,17 @@ public class RequestToKamap {
 		
 		HashMap<String, Object> map = new HashMap(); //결과 담을 map
 		
+		//Unirest 라이브러리를 이용해서 Http 요청을 보내서 접속
 		try {
 			String apiURL = "" + URLEncoder.encode("address", "UTF-8");
-			HttpResponse<JsonNode> response = Unirest.get(apiURL).header("Authorization", APIKey).asJson();
+			HttpResponse<JsonNode> response = Unirest.get(apiURL)				//해당 API URL에서
+												.header("Authorization", APIKey) // APIKey를 이용해서 권한을 얻는다.
+												.asJson();						// response는 JSon으로
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
+		
+		
+
 	}
 }
